63e2155c78901dc28fbd28457f901cf7510747a2
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-04-12 16:51:54 -0700

    Milestone 3

--- villagermap.cpp
+++ villagermap.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 void inc_friend(map<string, tuple<int, string, string>>&);
 void dec_friend(map<string, tuple<int, string, string>>&);
-void village_search(map<string, tuple<int, string, string>>, string);
+void village_search(map<string, tuple<int, string, string>>);
 void output(map<string, tuple<int, string, string>>);
 
 int main() {
     // declarations
-    map<string, tuple<int, string, string>> villagerColors;
+    map<string, tuple<int, string, string>> villagers;
 
     // insert elements into the map
     // note how the right-hand side of the assignment are the vector elements
-    villagerColors["Audie"] = {8, "frog", "bang!"};
-    villagerColors["Raymond"] = {9, "human", "dude!"};
-    villagerColors.insert({"Marshal", {4, "horse", "neigh!"}});
+    villagers["Audie"] = {8, "frog", "bang!"};
+    villagers["Raymond"] = {9, "human", "dude!"};
+    villagers.insert({"Marshal", {4, "horse", "neigh!"}});
 
     int ans = 0;
     while (ans != 4){
@@ -25,7 +25,10 @@ int main() {
         cout << "2. Decrease Friendship\n";
         cout << "3. Search for Villager\n";
         cout << "4. Exit\n";
-        if (ans == 1) inc_frin
+        cin >> ans;
+        if (ans == 1) inc_friend(villagers);
+        else if (ans == 2) dec_friend(villagers);
+        else if (ans == 3) village_search(villagers);
     }
 
 
@@ -34,19 +37,24 @@ int main() {
 
 void inc_friend(map<string, tuple<int, string, string>>& m){
     //using tuple member function, get, and increasing value by one
-    for (auto i: m){
-        get<0>(m[i.second])++;
-    }
+    string n;
+    cout << "Enter village name: ";
+    cin >> n;
+    get<0>(m[n])++;
     output(m);
 }
 void dec_friend(map<string, tuple<int, string, string>>& m){
     //using tuple member function, get, and decreasing value by one
-    for (auto i: m){
-        get<0>(m[i.second])--;
-    }
+    string n;
+    cout << "Enter village name: ";
+    cin >> n;
+    get<0>(m[n])--;
     output(m);
 }
-void village_search(map<string, tuple<int, string, string>> m, string searchKey){
+void village_search(map<string, tuple<int, string, string>> m){
+    string searchKey;
+    cout << "Enter village name: ";
+    cin >> searchKey;
     auto it = m.find(searchKey);
     if (it != m.end()) {  // the iterator points to beyond the end of the map if key not found
         cout << "\nFound " << searchKey;
